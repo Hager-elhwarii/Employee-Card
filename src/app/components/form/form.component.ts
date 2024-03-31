d66c872974bf3d2dfeba4df5e4d9fd0145767d0a
hager-dot <hagerelhwarii6633@gmail.com>
2024-04-01 00:28:34 +0200

    :sparkles: Add reactive form validation

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
-export class FormComponent {}
+export class FormComponent implements OnInit {
+  protected registerForm!: FormGroup;
+  protected submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      companyName: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-zA-Z ]*'),
+          Validators.minLength(5),
+        ],
+      ],
+      employeeName: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-zA-Z ]*'),
+          Validators.minLength(5),
+        ],
+      ],
+      idNumber: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      dateOfBirth: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  protected onSubmit(): void {
+    this.submitted = true;
+
+    if (this.registerForm.valid) {
+      console.table(this.registerForm.value);
+    }else {
+      console.log("form not valid");
+      
+    }
+  }
+
+  protected resetForm(): void {
+    this.registerForm.reset();
+  }
+}
+
