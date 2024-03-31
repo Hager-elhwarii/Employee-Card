1f09d738d0e75804671718a6388f789c286f9b99
hager-dot <hagerelhwarii6633@gmail.com>
2024-03-31 17:29:45 +0200

    :sparkles: Add employee card form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      "red-dark": "#9f1f31",
+      "red-lighter": "#cc2f45",
+      "gray-dark": "#2a2a2b",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+};
