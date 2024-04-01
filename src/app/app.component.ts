efd3f62ed6d68e89a9d5d04fa15f3e1bcd07384f
hager-dot <hagerelhwarii6633@gmail.com>
2024-04-01 05:19:11 +0200

    :sparkles: Add employee card both forward and backwards sides

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {FormComponent} from './components/form/form.component'
+import { FormComponent } from './components/form/form.component';
+import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , FormComponent],
+  imports: [FormComponent, EmployeeCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Employee-Card';
