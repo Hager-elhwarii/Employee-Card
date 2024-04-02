5e1ba0f95174611241fa05dc5da3b9114e08a404
hager-dot <hagerelhwarii6633@gmail.com>
2024-04-02 17:38:51 +0200

    :sparkles: Add dots loader after form submit

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -8,13 +8,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './loader.component.css',
 })
 export class LoaderComponent implements OnInit {
-  imagesLoaded: boolean = false;
-
-  ngOnInit() {
-    //   // Simulate image loading delay
-    setTimeout(() => {
-      this.imagesLoaded = true;
-    }, 2000); // Adjust the delay as per your requirement
-    this.imagesLoaded = false;
-  }
+  ngOnInit() {}
 }
